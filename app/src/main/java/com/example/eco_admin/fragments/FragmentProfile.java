ba8b664e8b4fa0351490c5dbb0a8ad25bd57d801
Aditya Sood <adityasood314@gmail.com>
2024-03-08 18:30:24 +0530

    UI : Create login screen

---
+++ app/src/main/java/com/example/eco_admin/fragments/FragmentProfile.java
@@ -0,0 +1,38 @@
+package com.example.eco_admin.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eco_admin.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentProfile#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentProfile extends Fragment {
+
+
+    public FragmentProfile() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
